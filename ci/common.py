83dbf41c20d3821a947538594607759163801cf2
KenYu <kenyu@improbable.io>
2020-08-07 17:44:14 +0800

    temp commit

--- ci/common.py
+++ ci/common.py
@@ -2,6 +2,8 @@
 import os
 import subprocess
 
+# For local debug without BuildKite
+DEBUG = True
 
 def log(msg, expand=False):
     if (expand):
@@ -20,7 +22,22 @@ def run_shell(cmds):
                             stderr=subprocess.PIPE)
 
 
+def set_buildkite_meta_data(name, value):
+    if DEBUG:
+        print('set meta-data %s=%s' % (name, value))
+    else:
+        args = [
+            'meta-data',
+            'set',
+            '"%s"' % name,
+            '"%s"' % value
+        ]
+        run_command('buildkite-agent', args)
+
+
 def get_buildkite_meta_data(name):
+    if DEBUG:
+        return ''
     cmds = [
         'buildkite-agent',
         'meta-data',
@@ -33,3 +50,22 @@ def get_buildkite_meta_data(name):
 
 def get_environment_variable(name, default_value):
     return os.environ.get(name) or default_value
+
+# For automatic pushd and popd
+class PushdContext:
+    cwd = None
+    original_dir = None
+
+    def __init__(self, dirname):
+        self.cwd = os.path.realpath(dirname)
+
+    def __enter__(self):
+        self.original_dir = os.getcwd()
+        os.chdir(self.cwd)
+        return self
+
+    def __exit__(self, type, value, tb):
+        os.chdir(self.original_dir)
+
+def pushd(dirname):
+    return PushdContext(dirname)
---
+++ ci/generate-auth-token.py
@@ -0,0 +1,60 @@
+# coding=utf-8
+
+import os
+import re
+import common
+from datetime import datetime
+
+def get_gdk_commit_hash(gdk_repo, gdk_branch_name):
+    cmds = [
+        'git',
+        'ls-remote',
+        '--head',
+        '\"%s\"' % gdk_repo,
+        '\"%s\"' % gdk_branch_name
+    ]
+    res = common.run_shell(cmds)
+    return res.stdout.read().decode('UTF-8')[0:6]
+
+
+def generate_auth_token(project_name):
+    common.pushd('spatial')
+    cmds = [
+        'spatial',
+        'project',
+        'auth',
+        'dev-auth-token',
+        'create',
+        '--description="Token generated for Example Project CI"',
+        '--project_name=%s' % project_name
+    ]
+    res = common.run_shell(cmds)
+    utf8_string = res.stdout.read().decode('UTF-8')
+    print(utf8_string)
+    url = re.findall(r'token_secret:\\"(.+)\\"', utf8_string)
+    print(url)
+    common.set_buildkite_meta_data('auth-token', url[0].decode('ASCII'))
+
+
+
+if __name__ == "__main__":
+    gdk_repo = common.get_environment_variable('GDK_REPOSITORY', 'git@github.com:spatialos/UnrealGDK.git')
+    gdk_branch_name = common.get_environment_variable('GDK_BRANCH', 'master')
+    project_name = common.get_environment_variable('SPATIAL_PROJECT_NAME', 'unreal_gdk')
+
+    common.log('get-gdk-head-commit')
+    gdk_commit_hash = get_gdk_commit_hash(gdk_repo, gdk_branch_name)
+    
+    common.log('generate-project-name')
+    engine_version_count = common.get_buildkite_meta_data('engine-version-count')
+    now = datetime.now()
+    for index in range(0, int(engine-version-count)):
+        index_str = '%d' % (index + 1)
+        name = 'deployment-name-%s' % index_str
+        value = 'exampleproject_%s_%s_%s' % (index_str, now.strftime('%m%d_%M%M%S'), gdk_commit_hash)
+        common.set_buildkite_meta_data(name, value)
+    
+    common.log('genreate-auth-token')
+    generate_auth_token(project_name)
+
+    exit(0)
