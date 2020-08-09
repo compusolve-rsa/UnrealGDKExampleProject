47ab2f9465c1b2de8ad48dcbd1d202498ec50e89
Ken.Yu <kenyu@improbable.io>
2020-08-09 16:16:44 +0800

    test both linux and windows

--- ci/slack-notify.ps1
+++ ci/slack-notify.ps1
@@ -2,15 +2,15 @@
 
 $gdk_branch_name = Get-Env-Variable-Value-Or-Default -environment_variable_name "GDK_BRANCH" -default_value "master"
 $launch_deployment = Get-Env-Variable-Value-Or-Default -environment_variable_name "START_DEPLOYMENT" -default_value "true"
-$slack_channel = Get-Env-Variable-Value-Or-Default -environment_variable_name "SLACK_CHANNEL" -default_value "#unreal-gdk-builds"
-$engine_version_count = buildkite-agent meta-data get "engine-version-count"
+$slack_channel = Get-Env-Variable-Value-Or-Default -environment_variable_name "SLACK_CHANNEL" -default_value "#ken-debug-message"
+$engine_version_count = "1"#buildkite-agent meta-data get "engine-version-count"
 $project_name = Get-Env-Variable-Value-Or-Default -environment_variable_name "SPATIAL_PROJECT_NAME" -default_value "unreal_gdk"
 $mac_build = Get-Env-Variable-Value-Or-Default -environment_variable_name "MAC_BUILD" -default_value "false"
 $firebase_test = Get-Env-Variable-Value-Or-Default -environment_variable_name "FIREBASE_TEST" -default_value "false"
-$gdk_commit_hash = buildkite-agent meta-data get "gdk_commit_hash"
+$gdk_commit_hash = "gdk_commit_hash"
 
 # Send a Slack notification with a link to the new deployment and to the build.
-Start-Event "slack-notify" "slack-notify"
+# Start-Event "slack-notify" "slack-notify"
     # Build Slack text
     if ($env:NIGHTLY_BUILD -eq "true") {
         $slack_text = ":night_with_stars: Nightly build of *Example Project* succeeded."
@@ -23,7 +23,9 @@ Start-Event "slack-notify" "slack-notify"
 
     # Read Slack webhook secret from the vault and extract the Slack webhook URL from it.
     $slack_webhook_secret = "$(imp-ci secrets read --environment=production --buildkite-org=improbable --secret-type=slack-webhook --secret-name=unreal-gdk-slack-web-hook)"
+    Write-Output $slack_webhook_secret
     $slack_webhook_url = $slack_webhook_secret | ConvertFrom-Json | %{$_.url}
+    Write-Output $slack_webhook_url
 
     $gdk_commit_url = "https://github.com/spatialos/UnrealGDK/commit/${gdk_commit_hash}"
     $project_commit_url = "https://github.com/spatialos/UnrealGDKExampleProject/commit/$env:BUILDKITE_COMMIT"
@@ -92,7 +94,7 @@ Start-Event "slack-notify" "slack-notify"
 
     if ($launch_deployment -eq "true") {
         for ($i = 0; $i -lt $engine_version_count; $i++){
-            $deployment_name = buildkite-agent meta-data get "deployment-name-$($i+1)"
+            $deployment_name = "deployment-name-$($i+1)"
             $deployment_url = "https://console.improbable.io/projects/${project_name}/deployments/${deployment_name}/overview"
             $deployment_button = @{
                                 type = "button"
@@ -107,4 +109,4 @@ Start-Event "slack-notify" "slack-notify"
     $json_request = $json_message | ConvertTo-Json -Depth 10
 
     Invoke-WebRequest -UseBasicParsing "$slack_webhook_url" -ContentType "application/json" -Method POST -Body "$json_request"
-Finish-Event "slack-notify" "slack-notify"
+# Finish-Event "slack-notify" "slack-notify"
